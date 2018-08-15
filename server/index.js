b80c6c1a7839479030eb72f7f80300069c6afcca
amirhemmat@gmail.com <Qwer!234>
2018-08-15 14:51:50 +0430

    Adds predeploy setup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const config = require('./config/dev');
-const Rental = require('./models/rental');
+const config = require('./config');
 const FakeDb = require('./fake-db');
+const path = require('path');
 
 const rentalRoutes = require('./routes/rentals'),
       userRoutes = require('./routes/users'),
@@ -12,8 +12,10 @@ const rentalRoutes = require('./routes/rentals'),
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB_URI , { useNewUrlParser: true }).then(() => {
-  const fakeDb = new FakeDb();
-  // fakeDb.seedDb();
+  if (process.env.NODE_ENV === 'production') {
+    const fakeDb = new FakeDb();
+    // fakeDb.seedDb();
+  }
 });
 
 const app = express();
@@ -23,6 +25,15 @@ app.use(bodyParser.json());
 app.use('/api/v1/rentals', rentalRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/bookings', bookingsRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  const appPath = path.join(__dirname, '..', 'dist', 'bwm-cli');
+  app.use(express.static(appPath));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(appPath, 'index.html'));
+  });
+}
 
 
 const PORT = process.env.PORT || 3001;
